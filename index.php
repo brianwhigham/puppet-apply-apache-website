6867448bcc87f22acfa4700dff489b6662302990
Brian Whigham <brian.whigham@usg.edu>
2015-03-16 23:12:11 -0400

    initial website

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="assets/css/main.css">
+  </head>
+  <body>
+    <center>
+      <h1>PeachNet&reg; Virtual Data Center</h1>
+      <font size=6>
+        Stop worrying about
+        <div id="news_ticker">
+          <!--<span>Latest News</span>-->
+          <ul>
+          <li><a href="#">this year's purchase order</a></li>
+          <li><a href="#">testing the backup generator</a></li>
+          <li><a href="#">maintenance contracts</a></li>
+          <li><a href="#">backup windows</a></li>
+          <li><a href="#">failing hard drives and mirrors</a></li>
+          <li><a href="#">cooling and power</a></li>
+          </ul>
+        </div>
+        And get back to 
+        <div id="news_ticker">
+          <ul>
+          <li><a href="#">making great applications!</a></li>
+          <li><a href="#">doing what you were hired to do!</a></li>
+          <li><a href="#">making your monitors better.</a></li>
+          <li><a href="#">a reasonable work week.</a></li>
+          </ul>
+        </div>
+      </font>
+      <p>
+        <font size=3>
+          <a href="http://www.usg.edu/peachnet/cloud/vdc">Try it today for free</a><br />
+          <a href="http://doc.the.usg.edu/virtualdatacenter/guides/">See how easy it is to get your cloud service going.</a>
+          <a href="https://www.hipchat.com/gwJ63S7in">Chat with us right now.</a>
+        </font>
+        Your browser, <?php =$_SERVER["HTTP_USER_AGENT"]; ?> is supported here.
+      </p>
+    </center>
+  </body>
+</html>
