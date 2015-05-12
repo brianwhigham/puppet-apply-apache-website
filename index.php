4460053873e470aae26c7179e23bf22e534d31ba
Brian Whigham <brian.whigham@usg.edu>
2015-05-11 23:18:48 -0400

    bootstrap

--- index.php
+++ index.php
@@ -1,41 +1,201 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <link rel="stylesheet" href="assets/css/main.css">
+    <meta charset="utf-8">
+    <title>PeachNet VDC - Automate Today</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Brian Whigham">
+
+    <!-- Le styles -->
+    <link href="../assets/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 20px;
+        padding-bottom: 60px;
+      }
+
+      /* Custom container */
+      .container {
+        margin: 0 auto;
+        max-width: 1000px;
+      }
+      .container > hr {
+        margin: 60px 0;
+      }
+
+      /* Main marketing message and sign up button */
+      .jumbotron {
+        margin: 80px 0;
+        text-align: center;
+      }
+      .jumbotron h1 {
+        font-size: 100px;
+        line-height: 1;
+      }
+      .jumbotron .lead {
+        font-size: 24px;
+        line-height: 1.25;
+      }
+      .jumbotron .btn {
+        font-size: 21px;
+        padding: 14px 24px;
+      }
+
+      /* Supporting marketing content */
+      .marketing {
+        margin: 60px 0;
+      }
+      .marketing p + h4 {
+        margin-top: 28px;
+      }
+
+
+      /* Customize the navbar links to be fill the entire space of the .navbar */
+      .navbar .navbar-inner {
+        padding: 0;
+      }
+      .navbar .nav {
+        margin: 0;
+        display: table;
+        width: 100%;
+      }
+      .navbar .nav li {
+        display: table-cell;
+        width: 1%;
+        float: none;
+      }
+      .navbar .nav li a {
+        font-weight: bold;
+        text-align: center;
+        border-left: 1px solid rgba(255,255,255,.75);
+        border-right: 1px solid rgba(0,0,0,.1);
+      }
+      .navbar .nav li:first-child a {
+        border-left: 0;
+        border-radius: 3px 0 0 3px;
+      }
+      .navbar .nav li:last-child a {
+        border-right: 0;
+        border-radius: 0 3px 3px 0;
+      }
+    </style>
+    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
   </head>
+
   <body>
-    <center>
-      <h1>PeachNet&reg; Virtual Data Center</h1>
-      <font size=6>
-        Stop worrying about
-        <div id="news_ticker">
-          <!--<span>Latest News</span>-->
-          <ul>
-          <li><a href="#">this year's purchase order</a></li>
+
+    <div class="container">
+
+      <div class="masthead">
+        <h3 class="muted">PeachNet&reg; Virtual Data Center</h3>
+        <div class="navbar">
+          <div class="navbar-inner">
+            <div class="container">
+              <ul class="nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="http://www.usg.edu/peachnet/cloud/vdc">About</a></li>
+                <li><a href="http://doc.the.usg.edu/virtualdatacenter/doc/">Documentation</a></li>
+                <li><a href="http://doc.the.usg.edu/virtualdatacenter/guides/">Demos & Guides</a></li>
+              </ul>
+            </div>
+          </div>
+        </div><!-- /.navbar -->
+      </div>
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1>Marketing stuff!</h1>
+        <p class="lead">Stop Fussing</p>
+        <a class="btn btn-large btn-success" href="http://www.usg.edu/peachnet/cloud/vdc">Get started today</a>
+      </div>
+
+      <hr>
+
+      <!-- Example row of columns -->
+      <div class="row-fluid">
+        <div class="span4">
+          <h2>Overview</h2>
+          <p>Deploy virtual machines in your public cloud. PeachNet’s Virtual Data Center service provides an ESX cluster environment using VMware’s Virtual Cloud Director (vCD) for University System of Georgia (USG) institutions. The service leverages technical support through a dedicated team of ITS system administrators.</p>
+          <p><a class="btn" href="http://www.usg.edu/peachnet/cloud/vdc">View details &raquo;</a></p>
+       </div>
+        <div class="span4">
+          <h2>No Need to Worry About</h2>
+          <p><ul>          <li><a href="#">this year's purchase order</a></li>
           <li><a href="#">testing the backup generator</a></li>
           <li><a href="#">maintenance contracts</a></li>
           <li><a href="#">backup windows</a></li>
           <li><a href="#">failing hard drives and mirrors</a></li>
           <li><a href="#">cooling and power</a></li>
-          </ul>
+          </ul></p>
+          <p><a class="btn" href="http://www.usg.edu/peachnet/cloud/vdc">View details &raquo;</a></p>
         </div>
-        And get back to 
-        <div id="news_ticker">
-          <ul>
+        <div class="span4">
+          <h2>Get Back To</h2>
+          <p><ul>
           <li><a href="#">making great applications!</a></li>
           <li><a href="#">doing what you were hired to do!</a></li>
           <li><a href="#">making your monitors better.</a></li>
           <li><a href="#">a reasonable work week.</a></li>
           </ul>
         </div>
-      </font>
-      <p>
-        <font size=3>
-          <a href="http://www.usg.edu/peachnet/cloud/vdc">Try it today for free</a><br />
-          <a href="http://doc.the.usg.edu/virtualdatacenter/guides/">See how easy it is to get your cloud service going.</a>
-          <a href="https://www.hipchat.com/gwJ63S7in">Chat with us right now.</a>
-        </font>
-        Your browser, <?php echo $_SERVER["HTTP_USER_AGENT"]; ?> is supported here.
-      </p>
-    </center>
+      </div>
+      <div class="row-fluid">
+        <div class="span4">
+          <h2>Flexible</h2>
+          <p>Billing on your preferred schedule; easy backups; bring your favorite OS</p>
+          <p><a class="btn" href="http://www.usg.edu/peachnet/cloud/vdc">View details &raquo;</a></p>
+       </div>
+        <div class="span4">
+          <h2>High Availability</h2>
+          <p>Load balancing, vSphere HA, Backup UPS and generator power, environmentals monitoring</p>
+          <p><a class="btn" href="http://www.usg.edu/peachnet/cloud/vdc">View details &raquo;</a></p>
+        </div>
+        <div class="span4">
+          <h2>Contact Us</h2>
+          <p>For information on ordering this service contact Andrea Wilson and our PeachNet Cloud Services team at
+<br />E-mail: cloud@usg.edu
+<br />Telephone: 706-583-2058
+<br /><a href="https://www.hipchat.com/gwJ63S7in">Hipchat</a></p>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="footer">
+        <p>Your browser, <?php echo $_SERVER["HTTP_USER_AGENT"]; ?> is supported here.</p>
+        <p>&copy; Board of Regents of the University System of Georgia 2013</p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap-transition.js"></script>
+    <script src="../assets/js/bootstrap-alert.js"></script>
+    <script src="../assets/js/bootstrap-modal.js"></script>
+    <script src="../assets/js/bootstrap-dropdown.js"></script>
+    <script src="../assets/js/bootstrap-scrollspy.js"></script>
+    <script src="../assets/js/bootstrap-tab.js"></script>
+    <script src="../assets/js/bootstrap-tooltip.js"></script>
+    <script src="../assets/js/bootstrap-popover.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+    <script src="../assets/js/bootstrap-collapse.js"></script>
+    <script src="../assets/js/bootstrap-carousel.js"></script>
+    <script src="../assets/js/bootstrap-typeahead.js"></script>
+
   </body>
 </html>
